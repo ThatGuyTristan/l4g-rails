b81b689939bf6c52cca487f47753084e84bb67b6
Bradley Ellis <bradleyaellis@gmail.com>
2023-12-12 14:29:39 -0600

    add player-systems relationship in player.rb

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,11 +2,14 @@ class Player < ApplicationRecord
   belongs_to :user
 
   has_and_belongs_to_many :games, :join_table => :player_games
+  has_many :systems
   
   scope :public_profile, -> { where(public: true) }
-  # TODO: Consult w/ srs on this
-  # scope :by_game, -> (game_id) { joins(:player_game).where(player_games: { game_id: game_id }) }
 
+  # TODO: finalize these scopes 
+  # scope :by_game, -> (game_id) { joins(:player_game).where(player_games: { game_id: game_id }) }
+  # scope :by_system, -> (system_id) { joins(:player_system).where(player_system: {system_id: system_id })) }
+  
   def email
     user.email
   end
